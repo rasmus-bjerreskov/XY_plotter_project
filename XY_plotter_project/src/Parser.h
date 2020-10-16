021abcd1800f6191cdfd0dd68a80b86fe871aa66
Rasmus Bjerreskov <rasmusst@metropolia.fi>
2020-10-16 16:58:33 +0300

    git fix 2

---
+++ XY_plotter_project/src/Parser.h
@@ -0,0 +1,56 @@
+/*
+ * Parser.h
+ *
+ *  Created on: 7.9.2020
+ *      Author: rqqht
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+#include "GcodePipe.h"
+#include "ParsedGdata.h"
+
+typedef struct Tokens {
+	int numTokens;
+	int currTokenNum;
+	char *currToken;
+} Tokens;
+
+class Parser {
+private:
+	#define MAX_CODE_LINE_LEN 100
+	char codeLine[MAX_CODE_LINE_LEN];
+	Tokens tokens;
+	GcodePipe *pipe;
+	static const char tokenDelimChar = ' ';
+	static const char lineEndChar = '\n';
+
+	char *tokenize();
+	char *nextToken(void);
+
+	bool gParser(ParsedGdata_t *data, char *tokLine);
+	bool mParser(ParsedGdata_t *data, char *tokLine);
+	bool gotoPositionParser(ParsedGdata_t *data, char *tokLine);
+	bool gotoOriginParser(ParsedGdata_t *data, char *tokLine);
+	bool comOpenParser(ParsedGdata_t *data, char *tokLine);
+	bool limitSwQueryParser(ParsedGdata_t *data, char *tokLine);
+	bool setPenPosParser(ParsedGdata_t *data, char *tokLine);
+	bool savePenUDPosParser(ParsedGdata_t *data, char *tokLine);
+	bool setLaserPowParser(ParsedGdata_t *data, char *tokLine);
+	bool saveStepperInfoParser(ParsedGdata_t *data, char *tokLine);
+
+	bool extractDirection(int *storage, char *tokLine, bool hasDelimChar = false, char delimChar = '\0');
+	bool extractInt(int *storage, char *tokLine, bool hasDelimChar = false, char delimChar = '\0');
+	bool extract8BitUint(int *storage, char *tokLine, bool hasDelimChar = false, char delimChar = '\0');
+
+	bool validateFloatStr(char *FloatStr, bool hasDelimChar = false, char delimChar = '\0');
+
+public:
+	Parser(GcodePipe *fetcher);
+	virtual ~Parser();
+
+	bool parse(ParsedGdata_t *_data);
+};
+
+#endif /* PARSER_H_ */
