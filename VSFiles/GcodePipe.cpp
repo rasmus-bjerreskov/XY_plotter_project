995f85bc16221b474779f3f267865d2ffcd27901
Rasmus Bjerreskov <rasmusst@metropolia.fi>
2020-10-28 17:42:33 +0200

    gitignore fix

--- VSFiles/GcodePipe.cpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "GcodePipe.h"
-
-GcodePipe::~GcodePipe() {
-
-}
--- XY_plotter_project/Debug/src/FileHandler.d
+++
@@ -1,7 +0,0 @@
-src/FileHandler.o src/FileHandler.d: ../src/FileHandler.cpp \
- ../src/FileHandler.h \
- C:\Users\Rasmus\Documents\MCUXpressoIDE_11.2.0_4120\XY_plotter_project\XY_plotter_project\inc/GcodePipe.h
-
-../src/FileHandler.h:
-
-C:\Users\Rasmus\Documents\MCUXpressoIDE_11.2.0_4120\XY_plotter_project\XY_plotter_project\inc/GcodePipe.h:
